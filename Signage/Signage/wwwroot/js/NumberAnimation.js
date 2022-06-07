646a7aca7c426782fffdbb30eef6372ee0c56867
Binh Dang <70655926+bddang23@users.noreply.github.com>
2022-06-07 15:50:38 -0400

    Add files via upload

---
+++ Signage/Signage/wwwroot/js/NumberAnimation.js
@@ -0,0 +1,96 @@
+﻿
+
+function NumAnimation() {
+	var STATUS_START = 1,
+		STATUS_MIDDLE = 2,
+		STATUS_END = 3,
+		TIMEOUT = 50,
+		TICKS = 5,
+		el = document.getElementById("num"),
+		value = el.dataset.value,
+		target = [],
+		dest = [],
+		status = STATUS_START,
+		ticks = 0,
+		index = 0,
+		random, handler, intervalId;
+
+	console.clear();
+
+	random = function () {
+		return Math.round(1 + Math.random() * 8);
+	};
+
+	handler = function () {
+		switch (status) {
+			case STATUS_START:
+				for (var i = 0; i <= index; i += 1) {
+					if (typeof target[i] === "number") {
+						dest[i] = random();
+					} else {
+						dest[i] = target[i];
+					}
+				}
+				break;
+
+			case STATUS_MIDDLE:
+				break;
+
+			case STATUS_END:
+				for (var i = index; i < target.length; i += 1) {
+					if (typeof target[i] === "number") {
+						if (i === index && ticks === TICKS - 1) {
+							dest[i] = target[i];
+						} else {
+							dest[i] = random();
+						}
+					}
+				}
+				break;
+		}
+
+		ticks += 1;
+
+		if (ticks >= TICKS) {
+			ticks = 0;
+
+			index += 1;
+
+			if (index >= target.length) {
+				index = 0;
+
+				switch (status) {
+					case STATUS_START:
+						status = STATUS_END; //STATUS_MIDDLE;
+						break;
+
+					case STATUS_MIDDLE:
+						status = STATUS_END;
+						break;
+
+					case STATUS_END:
+						clearInterval(intervalId);
+						break;
+				}
+			}
+		}
+
+		el.innerText = dest.join("");
+	};
+
+	value.split("").forEach(function (part, index) {
+		var int = parseInt(part);
+		target[index] = isNaN(int) ? part : int;
+	});
+
+	intervalId = setInterval(handler, TIMEOUT);
+};
+
+function loopFunction(delay, callback) {
+	var loop = function () {
+		callback();
+		setTimeout(loop, delay);
+	}; loop();
+};
+
+loopFunction(15000, NumAnimation);
---
+++ Signage/Signage/wwwroot/js/site.js
@@ -0,0 +1,77 @@
+﻿//reload page every hour
+$(document).ready(function () {
+    setInterval(function () { reload_page(); }, 60 * 60000);
+});
+
+function reload_page() {
+    window.location.reload(true);
+}
+
+function startTime() {
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    ap = (hr < 12) ? "AM" : "PM";
+    hr = (hr == 0) ? 12 : hr;
+    hr = (hr > 12) ? hr - 12 : hr;
+    //Add a zero in front of numbers<10
+    hr = checkTime(hr);
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay + ", " + curMonth + " " + curDay + ", " + curYear;
+    document.getElementById("date").innerHTML = date;
+
+    var time = setTimeout(function () { startTime() }, 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function StartScroll() {
+    var list = document.getElementsByClassName('table-responsive');
+
+    Array.prototype.forEach.call(list, element => {
+        pageScroll(element);
+    });
+}
+
+
+async function pageScroll(objDiv) {
+    while (true) {
+        objDiv.scrollTop = objDiv.scrollTop + 1;
+        if ((objDiv.scrollTop + objDiv.clientHeight) == objDiv.scrollHeight) {
+            objDiv.scrollTop = 0;
+        }
+
+        await sleep(25);
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+StartScroll();
+
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function () {
+    $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+}, 20000);
